b353a88bd4ea975d3991c9c59089cdd65152c6e8
krzbar <bartoszekkj@gmail.com>
2018-09-20 12:14:21 +0200

    Update test_linreg_s3.R
    Making passing the tests slightly easier, no need to keep exact count of spaces.

--- Testsuites/test_linreg_s3.R
+++ Testsuites/test_linreg_s3.R
@@ -20,7 +20,7 @@ test_that("print() works", {
   linreg_mod <- linreg(Petal.Length~Sepal.Width+Sepal.Length, data=iris)
 
   expect_output(print(linreg_mod),"linreg\\(formula = Petal\\.Length ~ Sepal\\.Width \\+ Sepal\\.Length, data = iris\\)")
-  expect_output(print(linreg_mod)," \\(Intercept\\)   Sepal\\.Width  Sepal\\.Length")
+  expect_output(print(linreg_mod),"( )*\\(Intercept\\)( )*Sepal\\.Width( )*Sepal\\.Length")
 })
 
 test_that("pred() works", {
