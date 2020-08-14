ff1556baa89c23171d31f2bed99bd1f8d960a3fb
krzbar <bartoszekkj@gmail.com>
2020-08-14 12:14:14 +0000

    Corrected deprecated code
    Correction of deprecated code by Martin Benes and Agustin Valencia

--- Labs/Tests/Tasks/approx_e_tests.R
+++ Labs/Tests/Tasks/approx_e_tests.R
@@ -3,13 +3,13 @@
 context("approx_e()")
 
 test_that("Kontroll av approx_e.", {
-  expect_that(exists("approx_e"), is_true(),
+  expect_that(exists("approx_e"), expect_true(),
               info = "Object approx_e() is missing")
   expect_that(approx_e, is_a("function"),
               info = "approx_e is not a function.")
-  expect_function_self_contained(object = approx_e,
-                                 "approx_e() contains undefined variables.")
-  expect_that(all(names(formals(approx_e)) %in% c("N")), condition=is_true(),
+  expect_self_contained(object = approx_e,
+                        "approx_e() contains undefined variables.")
+  expect_that(all(names(formals(approx_e)) %in% c("N")), condition=expect_true(),
               info = "Arguments in approx_e are missing or misspelled.")
   expect_equal(is.numeric(approx_e(2)), TRUE,
                info = "approx_e() do not return a numeric scalar.")
--- Labs/Tests/Tasks/dot_prod_tests.R
+++ Labs/Tests/Tasks/dot_prod_tests.R
@@ -3,15 +3,15 @@
 context("dot_prod()")
 
 test_that("dot_prod()", {
-  expect_that(exists("dot_prod"), is_true(), 
+  expect_that(exists("dot_prod"), expect_true(), 
               info = "Object dot_prod() is missing.")
   expect_that(dot_prod, is_a("function"),
               info = "dot_prod is not a function.")
-  expect_function_self_contained(object = dot_prod,
-                                 "dot_prod() contains undefined variables.")
-  expect_that(all(names(formals(dot_prod)) %in% c("a", "b")), condition=is_true(),
+  expect_self_contained(object = dot_prod,
+                        "dot_prod() contains undefined variables.")
+  expect_that(all(names(formals(dot_prod)) %in% c("a", "b")), condition=expect_true(),
               info = "The argument names are incorrect.")
-  expect_that(is.numeric(dot_prod(a=1:3, b=4:6)), condition=is_true(),
+  expect_that(is.numeric(dot_prod(a=1:3, b=4:6)), condition=expect_true(),
               info = "dot_prod() do not return a numeric value.")
   expect_equal(length(dot_prod(a=1:3, b=4:6)), 1,
               info = "dot_prod() do not return a scalar.")
