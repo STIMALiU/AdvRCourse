5e96a59c9d6b12921d00b1719e36f8b7ab62af35
krzbar <bartoszekkj@gmail.com>
2020-08-14 15:37:10 +0000

    Further correction
    Due to Martin Benes

--- Labs/Tests/Tasks/approx_e_tests.R
+++ Labs/Tests/Tasks/approx_e_tests.R
@@ -1,23 +1,21 @@
 ### Assignment : approx_e ###
 
 context("approx_e()")
-
 test_that("Kontroll av approx_e.", {
-  expect_that(exists("approx_e"), expect_true(),
+  expect_true(exists("approx_e"),
               info = "Object approx_e() is missing")
   expect_that(approx_e, is_a("function"),
               info = "approx_e is not a function.")
-  expect_self_contained(object = approx_e,
+  expect_function_self_contained(object = approx_e,
                         "approx_e() contains undefined variables.")
-  expect_that(all(names(formals(approx_e)) %in% c("N")), condition=expect_true(),
+  expect_true(all(names(formals(approx_e)) %in% c("N")),
               info = "Arguments in approx_e are missing or misspelled.")
   expect_equal(is.numeric(approx_e(2)), TRUE,
                info = "approx_e() do not return a numeric scalar.")
   expect_equal(approx_e(2), 2.5,
-              info = "approx_e() returns an erroneous result.")
+               info = "approx_e() returns an erroneous result.")
   expect_equal(approx_e(4), 2.708333, tolerance=0.0001,
                info = "approx_e() returns an erroneous result.")
   expect_equal(approx_e(100), exp(1), 
                info = "approx_e() returns an erroneous result.")
 })
-
--- Labs/Tests/Tasks/dot_prod_tests.R
+++ Labs/Tests/Tasks/dot_prod_tests.R
@@ -3,22 +3,21 @@
 context("dot_prod()")
 
 test_that("dot_prod()", {
-  expect_that(exists("dot_prod"), expect_true(), 
+  expect_true(exists("dot_prod"), 
               info = "Object dot_prod() is missing.")
   expect_that(dot_prod, is_a("function"),
               info = "dot_prod is not a function.")
-  expect_self_contained(object = dot_prod,
+  expect_function_self_contained(object = dot_prod,
                         "dot_prod() contains undefined variables.")
-  expect_that(all(names(formals(dot_prod)) %in% c("a", "b")), condition=expect_true(),
+  expect_true(all(names(formals(dot_prod)) %in% c("a", "b")),
               info = "The argument names are incorrect.")
-  expect_that(is.numeric(dot_prod(a=1:3, b=4:6)), condition=expect_true(),
+  expect_true(is.numeric(dot_prod(a=1:3, b=4:6)),
               info = "dot_prod() do not return a numeric value.")
   expect_equal(length(dot_prod(a=1:3, b=4:6)), 1,
-              info = "dot_prod() do not return a scalar.")
+               info = "dot_prod() do not return a scalar.")
   expect_equal(dot_prod(a=1:3, b=4:6), 32,
                info = "dot_prod() do not return a correct result.")
   expect_equal(dot_prod(a=5:10, b=20:25), 1030,
                info = "dot_prod() do not return a correct result.")
   
 })
-
