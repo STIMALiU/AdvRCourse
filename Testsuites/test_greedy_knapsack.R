3d28fdc70f0e493d5440df366759dfa38011e288
krzbar <bartoszekkj@gmail.com>
2021-08-17 11:05:37 +0000

    random number generation updated

--- Testsuites/test_greedy_knapsack.R
+++ Testsuites/test_greedy_knapsack.R
@@ -1,7 +1,8 @@
 
 context("greedy_knapsack")
 
-set.seed(42)
+suppressWarnings(RNGversion(min(as.character(getRversion()),"3.5.3")))
+set.seed(42, kind = "Mersenne-Twister", normal.kind = "Inversion")
 n <- 2000
 knapsack_objects <- data.frame(
   w=sample(1:4000, size = n, replace = TRUE),
